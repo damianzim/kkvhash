afa69042ff7c075e672579258b3dae581b46e539
Damian Zimon <zimondamian@gmail.com>
2020-03-17 20:49:11 +0100

    Add similarity functions for integers, use argparse

--- run.py
+++ run.py
@@ -1,8 +1,24 @@
+from argparse import (
+    ArgumentParser,
+    Namespace,
+)
 import datetime
 
 from kkvhash import kkv_hash
+from testing.similarity import sim_int_list_cross
 
-if __name__ == '__main__':
+
+def parse_args() -> Namespace:
+    parser = ArgumentParser()
+
+    parser.add_argument('--similarity', action='store_true', help='Print similarity')
+
+    return parser.parse_args()
+
+
+def main() -> None:
+    args = parse_args()
+
     data = [
         "a",
         "b",
@@ -39,10 +55,13 @@ if __name__ == '__main__':
 
     print(f"{'Hash dec':10} - {'Hash hex':8} - {'Time (us)':10} - Input hex")
 
+    hashes = []
+
     for key in data:
         start = datetime.datetime.now()
         temp_hash = kkv_hash(key.encode())
         time = datetime.datetime.now() - start
+        hashes.append(temp_hash)
         print(
             f"{temp_hash:10d}",
             '-',
@@ -53,3 +72,14 @@ if __name__ == '__main__':
             ' '.join(f"{ord(l):02X}" for l in key),
         )
 
+    if args.similarity:
+        print('Similarity:')
+        [
+            print(' '.join(f"{num:.2f}" for num in e))
+            for e
+            in sim_int_list_cross(hashes)
+        ]
+
+
+if __name__ == '__main__':
+    exit(main())
